ad2d31beb1ca9c938f0a298b083874f7781c2f4b
bmkim <bmkim@enliple.com>
2021-05-21 17:23:49 +0900

    js추가

--- www2/js/common.js
+++ www2/js/common.js
@@ -1,7 +1,6 @@
-let windowH
+var windowH = window.innerHeight;
 function fullPage(windowH){
-    windowH = window.innerHeight+'px';
-    document.querySelector(".fullPage").style.height = windowH;
+    document.getElementById("fullPage").style.height = windowH+'px';
 }
 $(document).ready(function(){
     fullPage(windowH);
@@ -9,3 +8,37 @@ $(document).ready(function(){
 $(window).resize(function(){
     fullPage(windowH);
 });
+
+function pageUp(){
+    window.scrollBy(0, windowH); 
+}
+
+function pageDown(){
+    window.scrollBy(0, -windowH); 
+}
+
+window.addEventListener('wheel',function(e){
+    console.log(e.deltaY)
+    if(e.deltaY > 0){
+        pageUp()
+    }else{
+        pageDown()
+    }
+
+})
+
+// $(window).bind('wheel DOMMouseScroll', function(event){
+//     var scTop = window.scrollTo();
+//     if (event.originalEvent.wheelDelta >= 0) {
+//         // scroll up
+//         console.log("스크롤 위로");
+        
+//     }
+//     else {
+        
+//         // scroll down
+//         if(scTop > 0){
+           
+//         }
+//     }
+// });
