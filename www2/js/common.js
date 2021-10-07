da9b4c39c0f56fd570bc7fdd1e463e8797d7d19a
kspring <kbmbo4268@gmail.com>
2021-10-08 01:18:17 +0900

    ㅋㅋㅋㅋㅋ

--- www2/js/common.js
+++ www2/js/common.js
@@ -1,15 +1,17 @@
-var windowH
-var menuList = document.querySelector('.menuList');
-var mobBtn = document.querySelector('.mNav');
-var header = document.getElementsByTagName('header')[0];
-var dim = document.querySelector('.dim');
-function fullPageH(){
+const menuList = document.querySelector('.menuList');
+const mobBtn = document.querySelector('.mNav');
+const header = document.getElementsByTagName('header')[0];
+const dim = document.querySelector('.dim');
+
+let windowH
+
+const fullPageH = () => {
     windowH = window.innerHeight;
     document.getElementById("fullPage").style.height = windowH +'px';
 }
 
-function fullPageS(e){
-    var mainBottom = document.getElementById("fullPage").getBoundingClientRect().bottom;
+const fullPageS = e => {
+    const mainBottom = document.getElementById("fullPage").getBoundingClientRect().bottom;
     windowH = window.innerHeight;
     if(e.deltaY > 0 && mainBottom == windowH){
         window.scrollTo(0, mainBottom);
@@ -23,9 +25,9 @@ function fullPageS(e){
     }
 }
 
-function navlink(e){
-    let scroll = e.target.dataset.scroll;
-    let scrollY = window.pageYOffset + document.getElementById(scroll).getBoundingClientRect().top;
+const navlink = e => {
+    const scroll = e.target.dataset.scroll;
+    const scrollY = window.pageYOffset + document.getElementById(scroll).getBoundingClientRect().top;
     if(scrollY != 0){
         header.classList.add('subNav');
         window.scrollTo(0,scrollY);
@@ -33,16 +35,16 @@ function navlink(e){
         window.scrollTo(0,0);
         header.classList.remove('subNav');
     }
-    removeClass()
+    removeClass();
 }
 
-function removeClass(){
+const removeClass = () => {
     mobBtn.classList.remove('on');
     menuList.classList.remove('down');
     dim.classList.remove('dimOn');
 }
 
-function mNav(){
+const mNav = () => {
     if(!mobBtn.classList.contains('on')){
         mobBtn.classList.add('on');
         menuList.classList.add('down');
@@ -54,132 +56,98 @@ function mNav(){
     }
 }
 
-document.addEventListener("DOMContentLoaded", function() {
-    fullPageH();
-    const tabItem = document.querySelectorAll(".tapBtn");
-    // const tabContent = document.querySelectorAll(".galleryCon");
-    // let activeCont = ''; // 현재 활성화 된 컨텐츠 (기본:#tab1 활성화)
-    for(let i = 0; i < tabItem.length; i++){
-        tabItem[i].addEventListener('click', function(e){
-        e.preventDefault();
-        for(let j = 0; j < tabItem.length; j++){
-            // 나머지 버튼 클래스 제거
-            tabItem[j].classList.remove('active');
-        }
-
-        // 버튼 관련 이벤트
-        this.classList.add('active');
-
-        // 버튼 클릭시 컨텐츠 전환
-        //   activeCont = this.getAttribute('href');
-        //   document.querySelector(activeCont).style.display = 'block';
-        });
-    }
+const filterBtn = function(){
+    const button = document.querySelectorAll('.tabBtn');
+    const storeItems = document.querySelectorAll('.galleryCon');
     
+    button.forEach(b=>b.addEventListener('click',(e)=>{
+        e.preventDefault()
+        const filter = e.target.dataset.filter
+
+        storeItems.forEach(i=>{
+            if(filter ==='all'){
+                i.classList.remove('none');
+
+            }else{
+                if(i.classList.contains(filter)){
+                    i.classList.remove('none');
+                }
+                else{
+                    i.classList.add('none');
+                }
+            }
+        })
+    })
+)
+}
+// const tabList = document.querySelectorAll('.tab_menu .list li');
+//   const contents = document.querySelectorAll('.tab_menu .cont_area .cont')
+//   let activeCont = ''; // 현재 활성화 된 컨텐츠 (기본:#tab1 활성화)
+
+//   for(var i = 0; i < tabList.length; i++){
+//     tabList[i].querySelector('.btn').addEventListener('click', function(e){
+//       e.preventDefault();
+//       for(var j = 0; j < tabList.length; j++){
+//         // 나머지 버튼 클래스 제거
+//         tabList[j].classList.remove('is_on');
+
+//         // 나머지 컨텐츠 display:none 처리
+//         contents[j].style.display = 'none';
+//       }
+
+//       // 버튼 관련 이벤트
+//       this.parentNode.classList.add('is_on');
+
+//       // 버튼 클릭시 컨텐츠 전환
+//       activeCont = this.getAttribute('href');
+//       document.querySelector(activeCont).style.display = 'block';
+//     });
+//   }
+const ani = e => {
+    windowH = window.innerHeight/2
+    const aniBox = document.querySelectorAll(".aniBox");
+    // for(let i = 0; i < aniBox.length; i++){
+    //     console.log((aniBox[i].getBoundingClientRect().top),windowH);
+    // }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    fullPageH();
 });
 
-window.onload = function (){
+window.onload = () => {
     setTimeout(window.scrollTo(0,0),300)
+    filterBtn();
 }
-// window.onbeforeunload = function() {
-    
-// };
+
 window.addEventListener('wheel', e => {
     fullPageS(e);
+    ani()
 });
+
 document.addEventListener("click", e => {
     null != e.target.dataset.scroll ? navlink(e) : null;
-    //mobBtn == e.target.parentElement ? mNav() : null;
     dim == e.target ? removeClass() : null;
 });
+
 mobBtn.addEventListener("click", e => {
     e.preventDefault();
     mNav()
 });
-window.addEventListener('resize', function() {
+
+window.addEventListener('resize', () => {
     fullPageH();
 });
+
 // window.addEventListener('touchstart', function () {
 //     window.pageYOffset == 0 ? header.classList.remove('subNav') : null;
 // });
-window.addEventListener('touchmove', function (e) {
+
+window.addEventListener('touchmove', e =>{
     dim == e.touches[0].target ? removeClass() : null;
     window.pageYOffset > 0 ? header.classList.add('subNav') : header.classList.remove('subNav');
 });
+
 // window.addEventListener('touchend', function (e){
 //     e.preventDefault();
 // });
-
-// HTML CSS JSResult Skip Results Iframe
-// EDIT ON
-// let slideUp = (target, duration=500) => {
-//     target.style.transitionProperty = 'height, margin, padding';
-//     target.style.transitionDuration = duration + 'ms';
-//     target.style.boxSizing = 'border-box';
-//     target.style.height = target.offsetHeight + 'px';
-//     target.offsetHeight;
-//     target.style.overflow = 'hidden';
-//     target.style.height = 0;
-//     target.style.paddingTop = 0;
-//     target.style.paddingBottom = 0;
-//     target.style.marginTop = 0;
-//     target.style.marginBottom = 0;
-//     window.setTimeout( () => {
-//       target.style.display = 'none';
-//       target.style.removeProperty('height');
-//       target.style.removeProperty('padding-top');
-//       target.style.removeProperty('padding-bottom');
-//       target.style.removeProperty('margin-top');
-//       target.style.removeProperty('margin-bottom');
-//       target.style.removeProperty('overflow');
-//       target.style.removeProperty('transition-duration');
-//       target.style.removeProperty('transition-property');
-//       //alert("!");
-//     }, duration);
-//   }
-// let slideDown = (target, duration=500) => {
-//     target.style.removeProperty('display');
-//     let display = window.getComputedStyle(target).display;
-
-//     if (display === 'none')
-//       display = 'block';
-
-//     target.style.display = display;
-//     let height = target.offsetHeight;
-//     target.style.overflow = 'hidden';
-//     target.style.height = 0;
-//     target.style.paddingTop = 0;
-//     target.style.paddingBottom = 0;
-//     target.style.marginTop = 0;
-//     target.style.marginBottom = 0;
-//     target.offsetHeight;
-//     target.style.boxSizing = 'border-box';
-//     target.style.transitionProperty = "height, margin, padding";
-//     target.style.transitionDuration = duration + 'ms';
-//     target.style.height = height + 'px';
-//     target.style.removeProperty('padding-top');
-//     target.style.removeProperty('padding-bottom');
-//     target.style.removeProperty('margin-top');
-//     target.style.removeProperty('margin-bottom');
-//     window.setTimeout( () => {
-//       target.style.removeProperty('height');
-//       target.style.removeProperty('overflow');
-//       target.style.removeProperty('transition-duration');
-//       target.style.removeProperty('transition-property');
-//     }, duration);
-//   }
-// let slideToggle = (target, duration = 500) => {
-//   if (window.getComputedStyle(target).display === 'none') {
-//     return slideDown(target, duration);
-//   } else {
-//     return slideUp(target, duration);
-//   }
-// }
-// // ====  
-// let speedAnimation = 400;
-// let targetId = document.getElementById("target");
-
-// let slideBtnClick = (cl, sl) => 
-// document.querySelector(cl).addEventListener('click', () => sl(targetId, speedAnimation));
-// slideBtnClick('.triggerToggle', slideToggle);
-// Resources1× 0.5× 0.25×Rerun
