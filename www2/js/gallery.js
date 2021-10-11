2e46199cfc23f604de08d6b454ab00a43f3d77cf
kspring <kbmbo4268@gmail.com>
2021-10-12 00:52:41 +0900

    거의 다했다

--- www2/js/gallery.js
+++ www2/js/gallery.js
@@ -1,14 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
     let filterList = ["all","javascript","react","vue","work"];
     let galleryList = [
-        {filter:"javascript",url:"",img:"../img/portfolio/1.jpg",tit:"javascript",txt:"개졸려토나와"},
-        {filter:"javascript",url:"",img:"../img/portfolio/1.jpg",tit:"javascript",txt:"개졸려토나와"},
-        {filter:"react",url:"",img:"../img/portfolio/2.jpg",tit:"react",txt:"개졸려토나와"},
-        {filter:"work",url:"",img:"../img/portfolio/4.jpg",tit:"work",txt:"개졸려토나와"},
-        {filter:"vue",url:"",img:"../img/portfolio/3.jpg",tit:"vue",txt:"개졸려토나와"},
-        {filter:"react",url:"",img:"../img/portfolio/2.jpg",tit:"react",txt:"개졸려토나와"},
-        {filter:"work",url:"",img:"../img/portfolio/4.jpg",tit:"work",txt:"개졸려토나와"},
-        {filter:"work",url:"",img:"../img/portfolio/4.jpg",tit:"work",txt:"개졸려토나와"}
+        {filter:"javascript",url:"",img:"./img/portfolio/1.jpg",tit:"타이틀",txt:"개졸려토나와"},
+        {filter:"javascript",url:"",img:"./img/portfolio/1.jpg",tit:"타이틀",txt:"개졸려토나와"},
+        {filter:"react",url:"",img:"./img/portfolio/2.jpg",tit:"타이틀",txt:"개졸려토나와"},
+        {filter:"work",url:"",img:"./img/portfolio/4.jpg",tit:"타이틀",txt:"개졸려토나와"},
+        {filter:"vue",url:"",img:"./img/portfolio/3.jpg",tit:"타이틀",txt:"개졸려토나와"},
+        {filter:"react",url:"",img:"./img/portfolio/2.jpg",tit:"타이틀",txt:"개졸려토나와"},
+        {filter:"work",url:"",img:"./img/portfolio/4.jpg",tit:"타이틀",txt:"개졸려토나와"},
+        {filter:"work",url:"",img:"./img/portfolio/4.jpg",tit:"타이틀",txt:"개졸려토나와"}
     ];
 
     const galleryCont = document.querySelector('.gallery-container');
@@ -17,19 +17,12 @@ document.addEventListener("DOMContentLoaded", () => {
         galleryCont.querySelector('.filterList').insertAdjacentHTML('beforeend', `<li type="button" data-filter="${t}">${t}</li>`);
     })
     galleryList.forEach( g => {
-        console.log(g.filter);
-        // galleryCont.querySelector('.gallery-list').insertAdjacentHTML('beforeend', `<figure class="galleryCon all ${g.filter}">
-        //     <div class="img" style="background-image:url('${g.img}')"></div>
-        //     <figcaption>
-        //         <h4>${g.tit}</h4>
-        //         <p type="button" onclick="window.open('${g.url}')">${g.txt}</p>
-        //     </figcaption>
-        // </figure>`);
-        galleryCont.querySelector('.gallery-list').insertAdjacentHTML('beforeend', `<figure class="galleryCon all ${g.filter}">
+        galleryCont.querySelector('.gallery-list').insertAdjacentHTML('beforeend', `<figure class="animated galleryCon all ${g.filter}">
             <div class="img" style="background-image:url('${g.img}')"></div>
             <figcaption>
-                <h4>${g.tit}</h4>
-                <p type="button" onclick="window.open('${g.url}')">${g.txt}</p>
+                <h4>${g.filter}</h4>
+                <p>${g.tit}</p>
+                <span type="button" onclick="window.open('${g.url}')" class="none" >${g.txt}</span>
             </figcaption>
         </figure>`);
     })
@@ -39,20 +32,24 @@ document.addEventListener("DOMContentLoaded", () => {
         filterBtn[0].classList.add('active');
         filterBtn.forEach( b => b.addEventListener('click', e => {
             e.preventDefault();
-            const filter = e.target.dataset.filter
+            const filter = e.target.dataset.filter;
             filterBtn.forEach( b => {
                 b.classList.remove('active');
             })
             e.target.classList.add('active');
             galleryCon.forEach( i => {
                 if(filter === 'all'){
-                    i.classList.remove('none');
+                    i.classList.add('zoomIn');
+                    i.classList.remove('fadeOutDown','none');
                 }else{
-                    if(i.classList.contains(filter)){
-                        i.classList.remove('none');
-                    } else{
+                    i.classList.add('fadeOutDown');
+                    setTimeout(function(){
                         i.classList.add('none');
-                    }
+                        if(i.classList.contains(filter)){
+                            i.classList.add('zoomIn');
+                            i.classList.remove('fadeOutDown','none');
+                        } 
+                    },500);
                 }
             })
         }))
