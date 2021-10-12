6b5b529713add3e88615fef1664b5b914aa776bd
kspring <kbmbo4268@gmail.com>
2021-10-12 14:37:16 +0900

    애미메이션 남음

--- www2/js/gallery.js
+++ www2/js/gallery.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
     let filterList = ["all","javascript","react","vue","work"];
     let galleryList = [
-        {filter:"javascript",url:"",img:"./img/portfolio/1.jpg",tit:"타이틀",txt:"개졸려토나와"},
+        {filter:"javascript",url:"",img:"./img/portfolio/1.jpg",tit:"타이틀",txt:"개졸려토나와개졸려토나와개졸려토나와개졸려토나와개졸려토나와개졸려토나와개졸려토나와개졸려토나와개졸려토나와개졸려토나와개졸려토나와개졸려토나와개졸려토나와개졸려토나와개졸려토나와개졸려토나와개졸려토나와"},
         {filter:"javascript",url:"",img:"./img/portfolio/1.jpg",tit:"타이틀",txt:"개졸려토나와"},
         {filter:"react",url:"",img:"./img/portfolio/2.jpg",tit:"타이틀",txt:"개졸려토나와"},
         {filter:"work",url:"",img:"./img/portfolio/4.jpg",tit:"타이틀",txt:"개졸려토나와"},
@@ -20,15 +20,16 @@ document.addEventListener("DOMContentLoaded", () => {
         galleryCont.querySelector('.gallery-list').insertAdjacentHTML('beforeend', `<figure class="animated galleryCon all ${g.filter}">
             <div class="img" style="background-image:url('${g.img}')"></div>
             <figcaption>
-                <h4>${g.filter}</h4>
-                <p>${g.tit}</p>
-                <span type="button" onclick="window.open('${g.url}')" class="none" >${g.txt}</span>
+                <h4>${g.tit}</h4>
+                <p>${g.filter}</p>
+                <span type="button" onclick="window.open('${g.url}')">${g.txt} <span>click!</span></span>
             </figcaption>
         </figure>`);
     })
-    const gallery = function(){
+    const gallery = () => {
         const filterBtn = document.querySelectorAll('.filterList li');
         const galleryCon = document.querySelectorAll('.galleryCon');
+        const popup = document.querySelector('.popup');
         filterBtn[0].classList.add('active');
         filterBtn.forEach( b => b.addEventListener('click', e => {
             e.preventDefault();
@@ -53,6 +54,18 @@ document.addEventListener("DOMContentLoaded", () => {
                 }
             })
         }))
+        galleryCon.forEach( p => p.addEventListener('click', () => {
+            if(popup.classList.contains('none')){
+                popup.classList.remove('none');
+                popup.insertAdjacentHTML('afterbegin', p.outerHTML);
+            } 
+        }))
+        popup.addEventListener('click', e => {
+            if(e.target === popup){
+                popup.classList.add('none');
+                popup.innerHTML='';
+            }
+        });
     }
     gallery();
 });
